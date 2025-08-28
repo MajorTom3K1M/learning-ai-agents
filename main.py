6e3276fd93912e0b10fd5eccba5e915a8db13d1e
MajorTom3K1M <invaderzgame@gmail.com>
2025-08-28 22:52:26 +0700

    feat: add verbose command

--- main.py
+++ main.py
@@ -10,23 +10,35 @@ api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
 def main():
-    if len(sys.argv) < 2:
-        print("Usage: python main.py <your_prompt>")
+    args = sys.argv[1:]
+
+    if not args:
+        print('Usage: python main.py "<your_prompt>" [--verbose]')
         sys.exit(1)
-    else:
-        user_prompt = " ".join(sys.argv[1:])
-        messages = [
-            types.Content(role=types.Role.USER, parts=[types.Part(text=user_prompt)])
-        ]
+
+    verbose = False
+    if args[-1] == "--verbose":
+        verbose = True
+        args = args[:-1]
+
+        if not args:
+            print('Usage: python main.py "<your_prompt>" [--verbose]')
+            sys.exit(1)
+
+    user_prompt = " ".join(args)
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)])
+    ]
+
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=messages,
+    )
+
+    if verbose:
         print("User prompt:", user_prompt)
-    
-        response = client.models.generate_content(
-            model="gemini-2.0-flash-001",
-            contents=messages,
-        )
         print("Prompt tokens:", response.usage_metadata.prompt_token_count)
         print("Response tokens:", response.usage_metadata.candidates_token_count)
-        print("Response:", response.text)
 
 
 
